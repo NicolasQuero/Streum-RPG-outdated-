ada5c80f0b2a7ec9b1afdbe1b0410d4768633f61
Loris-B <57706552+Loris-B@users.noreply.github.com>
2020-01-05 17:27:55 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "Board.h"
 #include "GameMap.h"
 #include "Jeu.h"
-#include "Entities/Pos.h"
 #include <windows.h>
 #pragma execution_character_set( "utf-8" )
 
 using namespace std;
 
+int deroulementJeu(Jeu *j,int k,int turn,Oueurj *O);
 
 
 int main() {
@@ -21,48 +21,41 @@ int main() {
     Jeu j;
     j.afficherMiniMap();
     int k=-1;//definit s'ou on viens
-    Oueurj O = Oueurj();
+    int turn=0;
+    Oueurj O;
     do{
-        int posX, posY;
-
-        GameMap Map(j.getNomMap());
-        Map.creaPorte(&j,1);
-
-        if(k==1){posY=Map.getHauteur()-2,posX=(Map.getLongueur())/2;} // on vient d'en haut, on cree le joueur en bas
-        else if(k==2){posY=1,posX=(Map.getLongueur())/2;}//... d'en bas,... en haut
-        else if(k==3){posY=(Map.getHauteur())/2,posX=(Map.getLongueur())-2;}//... de gauche,... a gauche
-        else if(k==4){posY=(Map.getHauteur())/2,posX=1;}//... de droite,... a droite
-        else if(k==-1){posY=(Map.getHauteur())/2,posX=(Map.getLongueur())/2;}
-        O.pos = Pos(posX, posY);
-        Board board = Board(Map,O);
-
-        board.printMap();
-        int turn = 0;
-        cout << endl << "****************** La partie débute, nous sommes au tour " << turn << " ******************" << endl << endl;
-        k = 0;
-        while (k == 0) {
-            k = board.playTurn();
-            turn ++;
-            cout << endl << "****************** Le tour " << turn << " est terminé. ******************" << endl << endl;
-            board.printMap();
-        }
-        switch (k) {
-       case 1:
-            j.deplacementMiniMapBas();
-            break;
-        case 2:
-            j.deplacementMiniMapHaut();
-            break;
-        case 3:
-            j.deplacementMiniMapDroite();
-            break;
-        case 4:
-            j.deplacementMiniMapGauche();
-            break;
-        }
-
+        deroulementJeu(&j,k,turn,&O);
     }while(j.getNomMap()!="fin");
     cout<<"vous avez gagner";
     return 0;
 }
 
+
+
+int deroulementJeu(Jeu *j,int k,int turn,Oueurj *O)
+{
+    GameMap Map(j->getNomMap());
+    Map.creaPorte(j,1);
+    int posXminiMap, posYminiMap;
+
+    if(k==0){posYminiMap=Map.getHauteur()-2,posXminiMap=(Map.getLongueur())/2;} // on vient d'en haut, on cree le joueur en bas
+    else if(k==1){posYminiMap=2,posXminiMap=(Map.getLongueur())/2;}//... d'en bas,... en haut
+    else if(k==2){posYminiMap=(Map.getHauteur())/2,posXminiMap=(Map.getLongueur())-2;}//... de gauche,... a gauche
+    else if(k==3){posYminiMap=(Map.getHauteur())/2,posXminiMap=1;}//... de droite,... a droite
+    else if(k==-1){posYminiMap=(Map.getHauteur())/2,posXminiMap=(Map.getLongueur())/2;}
+
+    O->pos=Pos(posYminiMap,posXminiMap);
+    Board board = Board(Map,*O);
+    board.printMap();
+    cout << endl << "****************** La partie débute, nous sommes au tour " << turn << " ******************" << endl << endl;
+    while (board.gameOn && board.playTurn()){
+        turn ++;
+        if (O->pos.y==Map.getHauteur()-2&&O->pos.x==(Map.getLongueur())/2){break;}
+        else if(O->pos.y==1&&O->pos.x==(Map.getLongueur())/2){break;}
+        else if (O->pos.y==1&&O->pos.x==(Map.getLongueur())/2){break;}
+        else if (O->pos.y==(Map.getHauteur())/2&&O->pos.x==(Map.getLongueur())/2){break;}
+        cout << endl << "****************** Le tour " << turn << " est terminé. ******************" << endl << endl;
+        board.printMap();
+    }
+        cout<<"ca marche";
+}
